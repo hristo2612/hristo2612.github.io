5216f047e81c513acf82b098915afe48b9212001
hristo2612 <hristoapps@gmail.com>
2022-04-05 16:42:50 +0300

    Create empty Nav & Main component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
+import { NavComponent } from './components/nav/nav.component';
+import { MainComponent } from './components/main/main.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    NavComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
