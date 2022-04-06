2eb63e59b65e047a855ba990bdced69f395c448e
hristo2612 <hristoapps@gmail.com>
2022-04-06 15:25:09 +0300

    Section Styling & Logic

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,11 +1,43 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface Section {
+  title: string;
+  entries: Entry[];
+}
+
+export interface Entry {
+  leftTitle: string;
+  leftContent: string;
+  rightTitle: string;
+  rightContent?: string;
+  rightContentList?: string[];
+}
+
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.scss']
 })
 export class MainComponent implements OnInit {
+
+  sections: Section[] = [
+    {
+      title: 'Work Experience',
+      entries: [
+        {
+          leftTitle: 'The Stars Group',
+          leftContent: 'Sept. 2018 - Present',
+          rightTitle: 'Senior Front-End Developer',
+          rightContent: 'Developing and maintaining the front-end of the company\'s website.',
+          rightContentList: [
+            'WOrked on integrating NASA equipment',
+            'Worked on integrating NASA equipment',
+            'Did some work on integrating NASA equipment'
+          ],
+        }
+      ]
+    }
+  ];
 
   constructor() { }
 
