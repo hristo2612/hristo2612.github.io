c00ecbb6c2e2d5c9bd11f66a5aa795c3fab78b9d
hristo2612 <hristoapps@gmail.com>
2025-03-08 01:00:26 +0200

    Refresh my Resume website

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -17,10 +17,9 @@ export interface Entry {
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
-  styleUrls: ['./main.component.scss']
+  styleUrls: ['./main.component.scss'],
 })
 export class MainComponent implements OnInit {
-
   sections: Section[] = [
     {
       title: 'Work Experience',
@@ -36,56 +35,69 @@ export class MainComponent implements OnInit {
             'Optimizing and refactoring legacy code.',
             'Introducing new technologies for building, testing and maintaining the applications, including Cypress, NGRX, ESLint, Karma, etc.',
             'Building and deploying 3+ applications at once using the Jenkins CI/CD pipeline.',
-            'Automation of tedious and time consuming tasks using Node.JS scripts & Custom Angular Schematics.'
-          ]
+            'Automation of tedious and time consuming tasks using Node.JS scripts & Custom Angular Schematics.',
+          ],
         },
         {
           leftTitle: 'Effortel',
           leftContent: 'May. 2018 - July. 2018',
           rightTitle: 'Front-End Developer',
-          rightContent: 'Worked on a MVNE project enabling mobile virtual network operators to offer services to their own customers.',
+          rightContent:
+            'Worked on a MVNE project enabling mobile virtual network operators to offer services to their own customers.',
           rightContentList: [
             'Implement project specific features using Sencha ExtJS and SCSS',
-            'Learn how MVNEs ( Mobile Virtual Network Enablers ) & MVNOs ( Mobile Virtual Network Operators ) work together to offer services to their customers.'
+            'Learn how MVNEs ( Mobile Virtual Network Enablers ) & MVNOs ( Mobile Virtual Network Operators ) work together to offer services to their customers.',
           ],
         },
         {
           leftTitle: 'Software Group',
           leftContent: 'Jan. 2017 - Apr. 2018',
           rightTitle: 'Full-Stack Developer',
-          rightContent: 'Help deliver a modern SaaS FinTech Platform designed for the Cloud and focused on providing financial technology for MFIs, small banks and other organizations.',
+          rightContent:
+            'Help deliver a modern SaaS FinTech Platform designed for the Cloud and focused on providing financial technology for MFIs, small banks and other organizations.',
           rightContentList: [
             'Use popular technologies like AngularJS, Node.js, Bootstrap 4, CouchDB and MongoDB.',
             'Understand the concept of microservices and how they work together to provide a scalable, reliable and secure solution.',
             'Work with a team of developers to build a solution that is easy to use, secure and reliable.',
-            'Get familiar with popular tools like VSTS, WebStorm IDE, Git, etc.'
+            'Get familiar with popular tools like VSTS, WebStorm IDE, Git, etc.',
           ],
-        }
-      ]
+        },
+      ],
     },
     {
       title: 'Projects',
       entries: [
         {
+          leftTitle: 'SQLNoir',
+          leftLink: 'https://github.com/hristo2612/sqlnoir',
+          leftContent: 'Feb. 2025',
+          rightTitle: 'React & Tailwind',
+          rightContent:
+            'A SQL Game where you solve mysteries and crimes made in React, Tailwind',
+        },
+        {
           leftTitle: 'My Resume',
           leftLink: 'https://github.com/hristo2612/hristo2612.github.io',
           leftContent: 'Apr. 2022',
           rightTitle: 'Angular & SCSS',
-          rightContent: 'It is a static resume website. The one you are reading right now.',
+          rightContent:
+            'It is a static resume website. The one you are reading right now.',
         },
         {
           leftTitle: 'Trillo',
           leftLink: 'https://github.com/hristo2612/trillo',
           leftContent: 'Mar. 2022',
           rightTitle: 'Angular & SCSS',
-          rightContent: 'A static hotel booking website, implementing responsive design with flexbox.',
+          rightContent:
+            'A static hotel booking website, implementing responsive design with flexbox.',
         },
         {
           leftTitle: 'Nature Tours',
           leftLink: 'https://github.com/hristo2612/nature-tours-website',
           leftContent: 'Feb. 2022',
           rightTitle: 'Angular & SCSS',
-          rightContent: 'Website implementing core features of SCSS ( mixins, variables, prefixes, etc. ) and visual design concepts using Angular 12+'
+          rightContent:
+            'Website implementing core features of SCSS ( mixins, variables, prefixes, etc. ) and visual design concepts using Angular 12+',
         },
         {
           leftTitle: 'Web Sockets',
@@ -99,28 +111,32 @@ export class MainComponent implements OnInit {
           leftLink: 'https://github.com/hristo2612/Needs-Tracker',
           leftContent: 'Sept. 2021',
           rightTitle: 'Ionic & Angular',
-          rightContent: 'An app that let\'s you track your needs for the day and keeps track of your history throughout time.',
+          rightContent:
+            "An app that let's you track your needs for the day and keeps track of your history throughout time.",
         },
         {
           leftTitle: 'Cmd Search',
           leftLink: 'https://github.com/hristo2612/Cmd-Search',
           leftContent: 'Jan. 2019',
           rightTitle: 'Electron & Angular',
-          rightContent: 'Add frequently used commands to your list and re-use them in the future, passing custom parameters.'
+          rightContent:
+            'Add frequently used commands to your list and re-use them in the future, passing custom parameters.',
         },
         {
           leftTitle: 'Automating Unit Tests',
           leftLink: 'https://github.com/hristo2612/automating-basic-ng-tests',
           leftContent: 'Nov. 2018',
           rightTitle: 'Node.js',
-          rightContent: 'Opinionated and customizable boilerplate for automating unit tests in Angular 2+'
+          rightContent:
+            'Opinionated and customizable boilerplate for automating unit tests in Angular 2+',
         },
         {
           leftTitle: 'Facebook Event Stats',
           leftLink: 'https://github.com/hristo2612/fb-event-stats',
           leftContent: 'Mar. 2018',
           rightTitle: 'Node.js',
-          rightContent: 'A script that crawls through a given facebook event and finds what percentage of guys & girls are attending ( Not working anymore ).',
+          rightContent:
+            'A script that crawls through a given facebook event and finds what percentage of guys & girls are attending ( Not working anymore ).',
         },
         {
           leftTitle: 'LogX',
@@ -128,8 +144,8 @@ export class MainComponent implements OnInit {
           leftContent: 'Oct. 2016',
           rightTitle: 'JavaScript',
           rightContent: 'Space shooter game made in pure JavaScript',
-        }
-      ]
+        },
+      ],
     },
     {
       title: 'Education',
@@ -139,22 +155,21 @@ export class MainComponent implements OnInit {
           leftLink: 'https://softuni.bg/',
           leftContent: 'Feb. 2016 - Nov. 2016',
           rightTitle: 'Javascript Applications',
-          rightContent: 'Received a recommendation for my first job due to my exceptionally good grades.'
+          rightContent:
+            'Received a recommendation for my first job due to my exceptionally good grades.',
         },
         {
           leftTitle: 'First English Language School (FELS)',
           leftLink: 'https://www.fels-sofia.org/',
           leftContent: '2011 - 2015',
           rightTitle: '',
-          rightContent: ''
-        }
-      ]
+          rightContent: '',
+        },
+      ],
     },
   ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
